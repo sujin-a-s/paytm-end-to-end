1e9b9828b211d1cb4dc576428c461844f144ec30
Sujin <sunilkumarsujin@gmail.com>
2024-07-03 16:33:23 +0530

    added workflow

--- apps/user-app/.eslintrc.js
+++ apps/user-app/.eslintrc.js
@@ -1,9 +1,18 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/next.js"],
+  extends: ["@repo/eslint-config/next.js",'turbo'],
+  plugins: ['turbo'],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
   },
+  overrides: [
+    {
+      files: ["./apps/user-app/**/*.tsx", "./apps/merchant-app/**/*.tsx"],
+      rules: {
+        "no-unused-vars": "off",
+      },
+    },
+  ],
 };
